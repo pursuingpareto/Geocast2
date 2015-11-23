661bfd7b3bf2177c0b7d436839a38bb356496cec
Andy Brown <andy@udacity.com>
2015-11-23 14:14:49 -0700

    Initial Commit

---
+++ Geocast2Tests/Geocast2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Geocast2Tests.swift
+//  Geocast2Tests
+//
+//  Created by Andrew Brown on 11/23/15.
+//  Copyright © 2015 Andrew Brown. All rights reserved.
+//
+
+import XCTest
+@testable import Geocast2
+
+class Geocast2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
