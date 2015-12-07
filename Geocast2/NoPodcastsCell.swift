e8f82aafcf509e9ae6a9ac6c345d197aa2c39e61
Andy Brown <andy@udacity.com>
2015-12-07 12:08:50 -0800

    add no subscriptions cell

--- Geocast2/NoPodcastsCell.swift
+++ Geocast2/NoPodcastsCell.swift
@@ -9,13 +9,7 @@
 import UIKit
 
 class NoPodcastsCell: UITableViewCell {
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-    }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
 //    @IBOutlet weak var noPodcastsLabel: UILabel!
     
 }
