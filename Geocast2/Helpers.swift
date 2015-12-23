995c61caeb120bbe27915a079cd25cd53256cd6a
Andy Brown <andy@udacity.com>
2015-12-23 11:16:13 -0800

    fix negative seconds remaining bug

--- Geocast2/Helpers.swift
+++ Geocast2/Helpers.swift
@@ -13,9 +13,9 @@ import MapKit
 extension CMTime {
     func asString() -> String {
         let totalSeconds = self.seconds
-        let hours = Int(floor(totalSeconds / 3600))
-        let mins = Int(floor((totalSeconds % 3600) / 60))
-        let secs = Int(floor((totalSeconds % 3600) % 60))
+        let hours = max(Int(floor(totalSeconds / 3600)), 0)
+        let mins = max(Int(floor((totalSeconds % 3600) / 60)), 0)
+        let secs = max(Int(floor((totalSeconds % 3600) % 60)), 0)
         if hours > 0 {
             return String(format: "%i:%02i:%02i", hours, mins, secs)
         } else {
